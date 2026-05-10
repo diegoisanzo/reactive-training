cb4e7e6480f3738cd8e625bb50f98f8c3a845d53
Diego I. Sanzo <diego.sanzo@gmail.com>
2026-05-09 22:39:30 -0300

    adds BookMapperTest

---
+++ src/test/java/ar/training/reactive/infrastructure/adapter/out/persistence/BookEntityFixture.java
@@ -0,0 +1,16 @@
+package ar.training.reactive.infrastructure.adapter.out.persistence;
+
+import ar.training.reactive.fixture.BookDtoFixture;
+
+public class BookEntityFixture {
+
+    public static BookEntity withDefaults() {
+        var bookDto = BookDtoFixture.withDefaults();
+
+        return new BookEntity(bookDto.id(),
+                bookDto.isbn(),
+                bookDto.title()
+        );
+    }
+
+}
---
+++ src/test/java/ar/training/reactive/infrastructure/adapter/out/persistence/BookMapperTest.java
@@ -0,0 +1,46 @@
+package ar.training.reactive.infrastructure.adapter.out.persistence;
+
+import ar.training.reactive.fixture.BookFixture;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mapstruct.factory.Mappers;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@ExtendWith(MockitoExtension.class)
+public class BookMapperTest {
+
+    private final BookMapper bookMapper;
+
+    public BookMapperTest() {
+        this.bookMapper = Mappers.getMapper(BookMapper.class);
+    }
+
+    @Test
+    void mapsToBookOk() {
+        var bookEntity = BookEntityFixture.withDefaults();
+
+        var book = bookMapper.toBook(bookEntity);
+
+        assertNotNull(book);
+
+        assertEquals(bookEntity.getId(), book.getId());
+        assertEquals(bookEntity.getIsbn(), book.getIsbn());
+        assertEquals(bookEntity.getTitle(), book.getTitle());
+    }
+
+    @Test
+    void mapsToBookEntityOk() {
+        var book = BookFixture.withDefaults();
+
+        var bookEntity = bookMapper.toBookEntity(book);
+
+        assertNotNull(bookEntity);
+
+        assertEquals(bookEntity.getId(), book.getId());
+        assertEquals(bookEntity.getIsbn(), book.getIsbn());
+        assertEquals(bookEntity.getTitle(), book.getTitle());
+    }
+}
